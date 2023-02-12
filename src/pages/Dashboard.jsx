044b6f8538366fcc4731064f7407d52a70efe92c
Olosko23 <oloogeorge633@gmail.com>
2023-02-12 23:19:19 +0300

    '12'

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../components/Sidebar';
 const Dashboard = () => {
   return (
     <div className="w-full h-full">
-      <div className="flex">
+      <div className="flex flex-col">
         <div className="basis-1/5"><Sidebar /></div>
         <div className="basis-4/5"><Feed /></div>
       </div>
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const NoPage = () => {
   return (
-    <div className="grid place-items-center">
+    <div className="w-full h-screen grid place-items-center">
       <section className="flex flex-col gap-3">
-        <h2 className="font-bold text-center text-6xl">ERROR: 404</h2>
-        <p className="font-semibold text-xl">Under Construction</p>
+        <h2 className="font-bold text-center text-6xl">ERROR 404</h2>
+        <p className="font-semibold text-xl text-center">Under Construction</p>
       </section>
     </div>
   )
